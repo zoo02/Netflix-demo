fa714596a99d2af8998ac7e6f3359ae2415050f3
JuYoung <rlaguswls9838@icloud.com>
2024-04-07 19:51:48 +0900

    포스터 이미지 수정중

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import Banner from './components/banner/Banner'
 
+
+// 1. 배너 만들기 => popular영화의 첫번째 item보여주기
+// 2. popular movie
+// 3. top rated movie
+// 4. upcoming movie
 const Homepage = () => {
   return (
-    <div>Homepage</div>
+    <div>
+      <Banner />
+    </div>
   )
 }
 
---
+++ src/pages/Homepage/components/banner/Banner.jsx
@@ -0,0 +1,38 @@
+import React from "react"
+import { usePopularMoviesQuery } from "../../../../hooks/usePopularMovies"
+import Spinner from "react-bootstrap/Spinner"
+import Alert from "react-bootstrap/Alert"
+import "./Banner.style.css"
+
+const Banner = () => {
+	const { data, isLoading, isError, error } = usePopularMoviesQuery()
+	console.log("DDd", data)
+	if (isLoading) {
+		return(
+		<h1>
+			Loading...
+		</h1>
+	)}
+	if (isError) {
+        return(
+		<Alert variant="danger">{error.message}</Alert>
+	)}
+	return (
+		<div
+			style={{
+				backgroundImage:
+					"url(" +
+					`https://media.themoviedb.org/t/p/w1920_and_h600_bestv2${data.results[0].poster_path}` +
+					")",
+			}}
+			className="banner"
+		>
+			<div className="text-white banner-text-area">
+				<h1>{data?.results[0].title}</h1>
+				<p>{data?.results[0].overview}</p>
+			</div>
+		</div>
+	)
+}
+
+export default Banner
